2ad26e4aea98c09572ca716f30d68f97f8d595bd
eduardo <edumi22x@gmail.com>
2022-05-22 20:47:12 -0300

    aaa

--- src/Componentes/Pages/Nosotros/Seccion3.js
+++ src/Componentes/Pages/Nosotros/Seccion3.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import {
+Principal,
+Secundario,
+Box1,
+Box2,
+Card1,
+Card2,
+Card3,
+Card4,
+BoxCards,
+TituloLogros,
+NumeroLogros,
+TituloPrincipalLogros,
+} from './Seccion3Elements'
 
 function Seccion3() {
   return (
     <>
-     
+     <Principal>
+         <Secundario>
+           <Box1>
+            <TituloPrincipalLogros className='tituloLogros'>Logros</TituloPrincipalLogros>
+           </Box1>
+           <Box2>
+               <BoxCards>
+                <Card1>                    
+                    <img className='logos-logros' alt='logros1' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1648648236/DESARROLLOSGRUPODELSUD/Group_97_zd0cxi.svg'/>
+                    <TituloLogros className='textoLogros'>Unidades vendidas</TituloLogros>
+                    <NumeroLogros class="counter" data-count="150">987</NumeroLogros>
+                </Card1>
+                <Card2>
+                    <img className='logos-logros ubicacion' alt='logros3' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1647542851/DESARROLLOSGRUPODELSUD/Group_100_zhcwaz.svg'/>
+                    <TituloLogros className='textoLogros'>Unidades disponibles</TituloLogros>
+                    <NumeroLogros class="counter" data-count="150">526</NumeroLogros>
+                </Card2>
+            </BoxCards>
+            <BoxCards>
+                <Card3>  
+                <img className='logos-logros' alt='logros2' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1647542862/DESARROLLOSGRUPODELSUD/Group_129_hmhnsp.svg'/>
+                    <TituloLogros className='textoLogros'>M² desarrollados</TituloLogros> 
+                    <NumeroLogros class="counter" data-count="150">6</NumeroLogros>  
+                </Card3>
+                <Card4>
+                    <img className='logos-logros calendario' alt='logros3' src='https://res.cloudinary.com/grupo-delsud/image/upload/v1647543194/DESARROLLOSGRUPODELSUD/Group_103_dn4m2u.svg'/>
+                    <TituloLogros className='textoLogros'>Unidades disponibles</TituloLogros>
+                    <NumeroLogros class="counter" data-count="150">3</NumeroLogros>
+                </Card4>
+            </BoxCards>
+           </Box2>
+         </Secundario>
+     </Principal>
     </>
   )
 }
--- src/Componentes/Pages/Nosotros/Seccion3Elements.js
+++ src/Componentes/Pages/Nosotros/Seccion3Elements.js
@@ -1,3 +1,102 @@
 import styled from 'styled-components'
 
 
+export const Principal = styled.div`
+width: 100%;
+height: 100vh;
+`
+export const Secundario = styled.div`
+width: 100%;
+height: 100vh;
+margin: auto;
+margin-top: 500px;
+background: #1D2142;
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+
+export const Box1 = styled.div`
+width: 50%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+
+export const Box2 = styled.div`
+width: 50%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+export const Card1 = styled.div`
+
+width: 50%;
+height: 25vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 106px;
+text-align: center;
+align-items: center;
+`
+export const Card2 = styled.div`
+
+width: 50%;
+height: 25vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 106px;
+text-align: center;
+align-items: center;
+`
+export const Card3 = styled.div`
+
+width: 50%;
+height: 25vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 106px;
+text-align: center;
+align-items: center;
+`
+export const Card4 = styled.div`
+
+width: 50%;
+height: 25vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 106px;
+text-align: center;
+align-items: center;
+`
+export const BoxCards = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+
+export const TituloLogros = styled.h2`
+font-size: 20px;
+color: white;
+font-family: Poppins;
+font-weight: 200;
+`
+
+export const NumeroLogros = styled.div`
+font-size: 45px;
+font-family: Poppins;
+font-weight: 200;
+color: white;
+`
+export const TituloPrincipalLogros = styled.h2`
+font-size: 30px;
+color: white;
+font-family: Poppins;
+font-weight: 500;
+`
