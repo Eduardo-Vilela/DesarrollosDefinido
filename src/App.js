2037afac71faa657ae20677afecb605bf1e50a40
Eduardo-Vilela <you@example.com>
2022-07-01 15:40:25 -0300

    asfasfa

--- src/App.js
+++ src/App.js
@@ -7,25 +7,14 @@ import "swiper/css/bundle";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "rsuite/dist/rsuite.min.css";
 
-import CustomCursor from 'custom-cursor-react';
-import 'custom-cursor-react/dist/index.css';
+
+
 
 function App() {
   return (
     <>
      <Rutas/>
-     <CustomCursor
-      targets={['.link', '.your-css-selector']}
-      customClass='custom-cursor'
-      dimensions={30}
-      fill='#FFF'
-      smoothness={{
-        movement: 0.2,
-        scale: 0.1,
-        opacity: 0.2,
-      }}
-      targetOpacity={0.5}
-    />
+     
     </>
   );
 }
--- src/Componentes/Pages/Home/Home.js
+++ src/Componentes/Pages/Home/Home.js
@@ -12,41 +12,12 @@ import Seccion6 from './Seccion6'
 import Seccion7 from './Seccion7'
 import Seccion8 from './Seccion8'
 import Seccion9 from './Seccion9'
-import AnimatedCursor from "react-animated-cursor"
+
 
 
 function Home() {
   return (
     <>
-    <AnimatedCursor
-      innerSize={5}
-      outerSize={5}
-      color='29, 33, 66'
-      outerAlpha={0.1}
-      innerScale={0.7}
-      outerScale={2}
-      outerStyle={{
-        border: '1px solid #1D2142',
-        mixBlendMode: 'exclusion'
-      }}
-      innerStyle={{
-        backgroundColor: '#1D2142'
-      }}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link',
-        'Modal'
-      ]}
-    />
     <NavbarTransparent/>
     <ModalInicio/>
      <Seccion1/>
